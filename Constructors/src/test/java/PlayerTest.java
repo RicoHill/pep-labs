ac5b3fea081d48e7a5dc1f756fafae15481a2046
RicoHill <javahill@yahoo.com>
2022-12-23 16:25:42 +0000

    Saved progress on Constructors 2022-12-23 16:25:42 +0000

--- Constructors/src/test/java/PlayerTest.java
+++ Constructors/src/test/java/PlayerTest.java
@@ -7,15 +7,33 @@ import org.junit.Test;
  * This is a harder problem.
  */
 public class PlayerTest {
+    private static final boolean Player = false;
     /**
      * If a new player is instantiated using the argument "Frodo", their name should be "Frodo",
      * and their level should be 1.
      */
+    private static final boolean Null = false;
     @Test
     public void player1ArgConstructorTest1(){
-        Player p = new Player("Frodo");
+        Player p = new Player(p.playerName:"Frodo");
         Assert.assertEquals(p.playerName, "Frodo");
         Assert.assertEquals(p.level, 1);
+        if(p.playerName == "Frodo")
+        if(p.level == 1);
+        boolean c = false; 
+        System.out.println(c);
+
+
+        
+        
+        
+        
+        System.out.println("Frodo" + p.level);
+    
+    }
+
+    private String playerName(String string) {
+        return null;
     }
 
     /**
@@ -25,9 +43,11 @@ public class PlayerTest {
     @Test
     public void player1ArgConstructorTest2(){
         Player p = new Player("Sam");
+        
         Assert.assertEquals(p.playerName, "Sam");
         Assert.assertEquals(p.level, 1);
-    }
+        
+        
     /**
      * If a new player is instantiated using the arguments "Gollum" and 5, their name should be "Gollum",
      * and their level should be 5.
@@ -37,5 +57,8 @@ public class PlayerTest {
         Player p = new Player("Gollum", 5);
         Assert.assertEquals(p.playerName, "Gollum");
         Assert.assertEquals(p.level, 5);
+        if(p.playerName == "Fredo" + 5);
+        boolean c = true;
+        System.out.println(c);
     }
 }
