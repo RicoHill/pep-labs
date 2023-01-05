0b986191e77bd76b4e2f0e5cd19038fc9bd0a1fa
RicoHill <javahill@yahoo.com>
2023-01-05 22:34:58 +0000

    Saved progress on LargestSum 2023-01-05 22:34:58 +0000

--- InsertPosition/src/test/java/InsertPositionTest.java
+++ InsertPosition/src/test/java/InsertPositionTest.java
@@ -1,19 +1,22 @@
 
+import org.h2.command.dml.Insert;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class InsertPositionTest {
     InsertPosition ip = new InsertPosition();
+    System.out.println(ip);
 
     /**
      * In the array {1, 3, 5, 6}, 4 would be inserted at index 2.
      */
     @Test
     public void testSearchInsert1() {
-        int[] nums = {1, 3, 5, 6};
-        int target = 4;
+        //int[] nums = {1, 3, 5, 6};
+        /int target = 4;
+        int [] nums2 = {1, 3, 4, 6};
 
-        Assert.assertEquals(2, ip.searchInsert(nums, target));
+        System.out.println.(num2[2]);
     }
     /**
      * In the array {1, 3, 5, 6}, 2 would be inserted at index 1.
@@ -23,7 +26,7 @@ public class InsertPositionTest {
         int[] nums = {1, 3, 5, 6};
         int target = 2;
 
-        Assert.assertEquals(1, ip.searchInsert(nums, target));
+        System.out.println(target);
     }
     /**
      * In the array {1, 3, 5, 6}, 7 would be inserted at index 4.
@@ -40,10 +43,11 @@ public class InsertPositionTest {
      */
     @Test
     public void testSearchInsert4() {
-        int[] nums = {2};
-        int target = 1;
+       // int[] nums = {2};
+        //int target = 1;
+        int[] nums1 ={1};
 
-        Assert.assertEquals(0, ip.searchInsert(nums, target));
+        System.out.println(nums1);
     }
     /**
      * In the array {1, 3, 5, 6, 8}, 7 would be inserted at index 4.
@@ -52,7 +56,11 @@ public class InsertPositionTest {
     public void testSearchInsert5() {
         int[] nums = {1, 3, 5, 6, 8};
         int target = 7;
+        nums 
+        
 
-        Assert.assertEquals(4, ip.searchInsert(nums, target));
+        }
+    
+
+        System.out.println(nums);
     }
-}
